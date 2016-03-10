4b8a72975128932373cbae72b4eb243fdb2b9c5b
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2016-03-10 23:01:17 +0100

    changes

--- src/Main.purs
+++ src/Main.purs
@@ -1,36 +1,82 @@
 module Main where
 
 import Prelude
-
-import Data.Array ((..))
-import Data.Foldable (fold, foldMap)
+import Control.Monad.Eff (Eff)
+import Data.Array ((..), snoc)
+import Data.Foldable (fold, foldMap, maximum)
 import Data.Int (toNumber)
 import Data.Maybe (Maybe(Just), maybe)
 import Data.Monoid (mempty)
+import Data.Ord (max)
 
-import Graphics.Canvas (getCanvasElementById, getContext2D)
-import Graphics.Drawing
+import Graphics.Canvas (getCanvasElementById, getContext2D, Context2D, Canvas)
+import Graphics.Drawing (Drawing, scale, translate)
 import Graphics.Pixel (enemy, ship, bullet, renderClean)
 
 import Signal
 import Signal.Time (every)
-import Signal.Keyboard (rightArrow, leftArrow)
+import Signal.Keyboard
 
-image =
-  let enemies = fold do
-        i <- 1..5
-        pure (translate (toNumber i * 15.0) 0.0 enemy)
-      player = translate 46.0 50.0 ship
-      bullets = fold do
-        i <- 1..8
-        pure (translate 50.0 (6.0 + (toNumber i * 5.0)) bullet)
-  in enemies <> bullets <> player
+type Player = Int
+type Bullet = { x :: Int, y :: Int }
+type State = { player :: Player, bullets :: Array Bullet, cooldown :: Int }
+type Input = { movement :: Int, fire :: Boolean }
+
+initialState :: State
+initialState = {player: 46, bullets: [], cooldown: 0}
+
+newBullet :: Int -> Bullet
+newBullet x = {x, y: 50}
+
+moveBullet :: Bullet -> Bullet
+moveBullet b = b { y = b.y - 1}
+
+step :: Input -> State -> State
+step {movement, fire} {player, bullets, cooldown} =
+  let newPlayer = player + movement
+      newBullets =
+        map moveBullet bullets ++ if firing
+                                  then [newBullet (player + 4)]
+                                  else []
+      firing = fire && cooldown == 0
+      newCooldown = if firing
+                    then 25
+                    else max 0 (cooldown - 1)
+
+  in { player: newPlayer
+     , bullets: newBullets
+     , cooldown: newCooldown
+     }
+
+renderState :: State -> Drawing
+renderState {player, bullets} =
+  let enemies = foldMap renderEnemy (1..5)
+      player' = translate (toNumber player) 50.0 ship
+      bullets' = foldMap renderBullet bullets
+      renderBullet {x, y} = translate (toNumber x) (toNumber y) bullet
+      renderEnemy x = translate (toNumber x * 15.0) 0.0 enemy
+  in enemies <> bullets' <> player'
+
+input :: Boolean -> Boolean -> Boolean -> {movement :: Int, fire :: Boolean}
+input true false fire  = {movement: -1, fire}
+input false false fire = {movement:  0, fire}
+input true true fire   = {movement:  0, fire}
+input false true fire  = {movement:  1, fire}
+
+view :: forall e. Context2D -> State -> Eff (canvas :: Canvas | e) Unit
+view ctx state =
+  renderClean ctx $
+    translate 150.0 150.0 $
+      scale 5.0 5.0 $
+        renderState state
 
 main = do
   Just canvas <- getCanvasElementById "canvas"
   ctx <- getContext2D canvas
+  rArrow <- rightArrow
+  lArrow <- leftArrow
+  sbar <- spacebar
 
-  renderClean ctx $
-    translate 150.0 150.0 $
-      scale 5.0 5.0 $
-        image
+  let inputs = sampleOn (every 20.0) (input <~ lArrow ~ rArrow ~ sbar)
+      states = foldp step initialState inputs
+  runSignal $ view ctx <~ states
--- src/Signal/Keyboard.purs
+++ src/Signal/Keyboard.purs
@@ -1,8 +1,11 @@
 module Signal.Keyboard where
 
+import Prelude
+
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
 import DOM (DOM)
-import Signal (Signal)
+import Signal (Signal, runSignal)
 import Signal.DOM (keyPressed)
 
 rightArrow :: forall e. Eff (dom :: DOM | e) (Signal Boolean)
@@ -10,3 +13,12 @@ rightArrow = keyPressed 39
 
 leftArrow :: forall e. Eff (dom :: DOM | e) (Signal Boolean)
 leftArrow = keyPressed 37
+
+spacebar :: forall e. Eff (dom :: DOM | e) (Signal Boolean)
+spacebar = keyPressed 32
+
+logSignal :: forall a e. Signal a -> Eff (console :: CONSOLE | e) Unit
+logSignal s = runSignal $ Control.Monad.Eff.Console.Unsafe.logAny <$> s
+
+hole :: forall a. a
+hole = Unsafe.Coerce.unsafeCoerce unit
